5bfa2ecd60439c0d3da0f18ab98fb556ae44f3fa
Aaron Darling <aaron.darling@uts.edu.au>
2009-03-28 02:13:41 +0000

    updating usage of autotools, better default optimization

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
-(libtoolize || glibtoolize) && \
-aclocal -I m4 && \
-echo "Ignore any messages about adding libtool.m4 to aclocal.m4" && \
-automake -a && \
-autoconf && \
+autoreconf --force --install -I config  -I m4
 echo "Now run ./configure --prefix=$HOME && make install" && \
 echo "Add --disable-shared to the configure line if building on Mac OS X"
 
