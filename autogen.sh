c1f13135175e3a6756287375c1d6e00eeaab712c
Aaron Darling <aaron.darling@uts.edu.au>
2009-10-07 20:09:43 +0000

    reverting to previous autogen for OS X 10.4 compatibility

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-autoreconf --force --install -I config  -I m4
+(libtoolize || glibtoolize) && \
+aclocal -I m4 && \
+echo "Ignore any messages about adding libtool.m4 to aclocal.m4" && \
+automake -a && \
+autoconf && \
 echo "Now run ./configure --prefix=$HOME && make install" && \
 echo "Add --disable-shared to the configure line if building on Mac OS X"
 
